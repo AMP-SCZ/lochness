41e717e402d44ff5735aa764b491bd65dc7f96f7
Dheshan Mohandass <dheshan@mohandass.com>
2024-07-26 14:56:53 -0400

    feat.: Add remote metadata to track fileds like owner, modifier, last_modified
    Signed-off-by: Dheshan Mohandass <dheshan@mohandass.com>

--- lochness/db/models/remote_files.py
+++ lochness/db/models/remote_files.py
@@ -4,6 +4,7 @@ RemoteFile Model
 """
 
 from datetime import datetime
+from typing import Dict
 
 from lochness import db
 
@@ -17,7 +18,12 @@ class RemoteFile:
     """
 
     def __init__(
-        self, file_path: str, remote_name: str, hash_val: str, last_checked: datetime
+        self,
+        file_path: str,
+        remote_name: str,
+        hash_val: str,
+        last_checked: datetime,
+        remote_metadata: Dict[str, str],
     ):
         """
         Initialize a RemoteFile object.
@@ -27,11 +33,14 @@ class RemoteFile:
             remote_name (str): The name of the remote system.
             hash_val (str): The hash value of the file,
                 as provided by the remote system.
+            last_checked (datetime): The last time the file was checked.
+            remote_metadata (Dict[str, str]): Metadata about the file,
         """
         self.file_path = file_path
         self.remote_name = remote_name
         self.hash_val = hash_val
         self.last_checked = last_checked
+        self.remote_metadata = remote_metadata
 
     def __str__(self):
         """
@@ -56,6 +65,7 @@ class RemoteFile:
             r_remote_name TEXT NOT NULL,
             r_hash_val TEXT NOT NULL,
             r_last_checked TIMESTAMP NOT NULL,
+            r_remote_metadata JSONB,
             PRIMARY KEY (file_path, remote_name)
         );
         """
@@ -79,7 +89,7 @@ class RemoteFile:
         Return the SQL query to find matching remote_files by hash.
         """
         sql_query = f"""
-        SELECT r_file_path, r_remote_name, r_hash_val, r_last_checked
+        SELECT r_file_path, r_remote_name, r_hash_val, r_last_checked, r_remote_metadata
         FROM remote_files
         WHERE r_hash_val = '{hash_val}';
         """
@@ -99,14 +109,17 @@ class RemoteFile:
         hash_val = db.santize_string(self.hash_val)
         last_checked = db.santize_string(self.last_checked)
 
+        metadata = db.sanitize_json(self.remote_metadata)
+
         sql_query = f"""
             INSERT INTO remote_files (
-                r_file_path, r_remote_name, r_hash_val, r_last_checked
+                r_file_path, r_remote_name, r_hash_val, r_last_checked, r_remote_metadata
             ) VALUES (
-                '{file_path}', '{remote_name}', '{hash_val}', '{last_checked}'
+                '{file_path}', '{remote_name}', '{hash_val}', '{last_checked}', '{metadata}'
             ) ON CONFLICT (file_path, remote_name) UPDATE SET
                 r_hash_val = excluded.r_hash_val,
-                r_last_checked = excluded.r_last_checked;
+                r_last_checked = excluded.r_last_checked,
+                r_remote_metadata = excluded.r_remote_metadata;
         """
 
         return sql_query
